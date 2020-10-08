6c9442ba09d2d63cc36a62ed78d5845a4c687460
JJohn <jjohn@jjohns-laptop.home>
2020-10-08 13:10:20 -0400

    US10 Refactor
    Co-authored-by: Matthieu Dupuy <matt3327@gmail.com>

--- parser.py
+++ parser.py
@@ -245,17 +245,17 @@ def US09_check_child_birth_before_marriage_anomaly(fam,child):
     fam.anomalies.append(child.Id + " born before parents married")    
 
 # Matt
-def US10_check_marriage_after_14_anomaly(fam,wife,husband):
-  day1 = wife.birthDateObject
+def US10_check_marriage_after_14_anomaly(fam):
+  day1 = fam.wifeObject.birthDateObject
   day2 = fam.marriageDateObject 
-  day3 = (abs((day2 - day1).days)/365)
-  day4 = husband.birthDateObject
+  day3 = (((day2 - day1).days)/365)
+  day4 = fam.husbandObject.birthDateObject
   day5 = fam.marriageDateObject 
-  day6 = (abs((day5 - day4).days)/365)
+  day6 = (((day5 - day4).days)/365)
   if day3 < 14:
-    fam.errors.append("Wife married before 14 error")
+    fam.anomalies.append("Wife married before 14 anomaly")
   if day6 < 14:
-    fam.errors.append("Husband married before 14 error")
+    fam.anomalies.append("Husband married before 14 anomaly")
   # if int(wife.birthDateObject) - int(fam.marriageDateObject) > 14 and int(husband.birthDateObject) - int(fam.marriageDateObject) >14:
   #   fam.errors.append("error marriage before 14")
 
@@ -280,7 +280,7 @@ def check_families_for_errors_and_anomalies():
       US06_check_divorce_before_spouse_death_error(fam, husband, wife)
     US02_birth_before_marriage_error(fam,husband,wife)
     US04_check_marriage_before_spouse_death_error(fam,husband,wife)
-    US10_check_marriage_after_14_anomaly(fam,wife,husband)
+    US10_check_marriage_after_14_anomaly(fam)
     for child_id in fam.childrenIds:
       child = get_individual_by_id(child_id)
       US09_check_child_birth_before_marriage_anomaly(fam,child)
--- test_US10_marriage_after_14.py
+++ test_US10_marriage_after_14.py
@@ -7,37 +7,56 @@ from parser import US10_check_marriage_after_14_anomaly
 
 class Test(unittest.TestCase):
     def test_wifeYoungerThan14(self):
-        wife = Individual("I1")
+        wife = Individual("I1")  
         husband = Individual("I2")
         fam = Family("F1")
-        wife.birthDateObject = datetime(2000, 9,14)
-        husband.birthDateObject = datetime(1996,9,14)
+        fam.wifeObject = wife
+        fam.husbandObject = husband
+        fam.wifeObject.birthDateObject = datetime(2000,9,14)
+        fam.husbandObject.birthDateObject = datetime(1996,9,14)
         fam.marriageDateObject = datetime(2013, 9 ,14)
-        US10_check_marriage_after_14_anomaly(fam,wife,husband)
-        self.assertEqual(len(fam.errors), 1)
-        self.assertEqual(fam.errors, ["Wife married before 14 error"])  
+        US10_check_marriage_after_14_anomaly(fam)
+        self.assertEqual(len(fam.anomalies), 1)
+        self.assertEqual(fam.anomalies, ["Wife married before 14 anomaly"])  
 
     def test_husbandYoungerThan14(self):
         wife = Individual("I1")
         husband = Individual("I2")
         fam = Family("F1")
-        wife.birthDateObject = datetime(1992, 9,14)
-        husband.birthDateObject = datetime(2003,9,14)
+        fam.wifeObject = wife
+        fam.husbandObject = husband
+        fam.wifeObject.birthDateObject = datetime(1992,9,14)
+        fam.husbandObject.birthDateObject = datetime(2003,9,14)
         fam.marriageDateObject = datetime(2013, 9 ,14)
-        US10_check_marriage_after_14_anomaly(fam,wife,husband)
-        self.assertEqual(len(fam.errors), 1)
-        self.assertEqual(fam.errors, ["Husband married before 14 error"]) 
+        US10_check_marriage_after_14_anomaly(fam)
+        self.assertEqual(len(fam.anomalies), 1)
+        self.assertEqual(fam.anomalies, ["Husband married before 14 anomaly"]) 
 
     def test_bothOlderThan14(self):   
         wife = Individual("I1")
         husband = Individual("I2")
         fam = Family("F1")
-        wife.birthDateObject = datetime(1992, 9,14)
-        husband.birthDateObject = datetime(1995,9,14)
+        fam.wifeObject = wife
+        fam.husbandObject = husband
+        fam.wifeObject.birthDateObject = datetime(1992,9,14)
+        fam.husbandObject.birthDateObject = datetime(1995,9,14)
         fam.marriageDateObject = datetime(2013, 9 ,14)
-        US10_check_marriage_after_14_anomaly(fam,wife,husband)
-        self.assertEqual(len(fam.errors), 0)
-        self.assertEqual(fam.errors, [])  
-
+        US10_check_marriage_after_14_anomaly(fam)
+        self.assertEqual(len(fam.anomalies), 0)
+        self.assertEqual(fam.anomalies, [])  
+   
+    def test_bothYoungerThan14(self):   
+        wife = Individual("I1")
+        husband = Individual("I2")
+        fam = Family("F1")
+        fam.wifeObject = wife
+        fam.husbandObject = husband
+        fam.wifeObject.birthDateObject = datetime(2000,9,14)
+        fam.husbandObject.birthDateObject = datetime(2000,9,14)
+        fam.marriageDateObject = datetime(2013,9,14)
+        US10_check_marriage_after_14_anomaly(fam)
+        self.assertEqual(len(fam.anomalies), 2)
+        self.assertEqual(fam.anomalies, ["Wife married before 14 anomaly", "Husband married before 14 anomaly"])  
+    
 if __name__ == "__main__": 
     unittest.main()
